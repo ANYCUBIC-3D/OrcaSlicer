e31fc8f391ff115ff2c3fdb7669a52b8c97fabdb
cl <liuchunlin@anycubic.com>
2024-06-19 10:11:04 +0800

    Fix Linux Debug Compilation Issues

--- deps/GLFW/GLFW.cmake
+++ deps/GLFW/GLFW.cmake
@@ -9,7 +9,7 @@ endif()
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
     set(_glfw_use_wayland "-DGLFW_USE_WAYLAND=ON")
 else()
-    set(_glfw_use_wayland "-DGLFW_USE_WAYLAND=FF")
+    set(_glfw_use_wayland "-DGLFW_USE_WAYLAND=OFF")
 endif()
 
 orcaslicer_add_cmake_project(GLFW
